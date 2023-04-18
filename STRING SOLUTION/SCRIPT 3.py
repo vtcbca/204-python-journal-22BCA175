01168b716ed8d23a58f98b4a891e1cf836116129
22BCA175 <127925536+22BCA175@users.noreply.github.com>
2023-04-18 16:46:27 +0530

    Add files via upload

---
+++ STRING SOLUTION/SCRIPT 3.py
@@ -0,0 +1,18 @@
+ch='Y'
+l=[]
+while ch=='Y' or ch=='y':
+    n=input("Enter a name:")
+    l.append(n)
+    ch=input("\nDo you want to enter more name? (Yes=y/Y,No=N/n):")
+
+c=0
+n=[]
+for i in l:
+    a=len(i)
+    if a%2==0:
+        c=c+1
+        n.append(i)
+print("Total string of even number of character:{}".format(c))
+for j in n:
+    print(j)
+
